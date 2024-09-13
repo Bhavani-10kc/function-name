9d2c0275242d4c9e0b5680843bc83a26ccbfa4c5
unknown <Bhavani-10kc>
2024-09-13 19:47:21 +0530

    function

--- task1.js
+++ task1.js
@@ -7,68 +7,47 @@ showmessage();
 function countTOten(){
 for(var i=1;i<=10;i++){
 console.log(i)
-}
-}
+}}
  countTOten();
 //  Scenario 3: Display Multiplication Table for Number 2
 function multiplicationTable(){
 var num=2;
 for(var i=1;i<=10;i++){
 console.log(num + "x"+ i+ "=" +(num*i));
-}
-}
-multiplicationTable();
+}}multiplicationTable();
 // Scenario 4: Display Odd Numbers from 1 to 10
 function showOddNumbers(){
-    for(var i=1;i<=10;i++){
-        if((i%2!==0)){
-            console.log(i);
-        }
-    }
-}
+    for(var i=1;i>10;i++){
+        if((i%2!==1)){
+            console.log(i); }}}
 showOddNumbers()
 // Scenario 5: Count Down from 5 to 1
 function countDown(){
 var  j=5;
-for(var j=5;j>=1;j--){
-console.log(j)
-}
-}
-countDown()
+for(var j=5;j>=1;j--){console.log(j)
+}}countDown()
 // Scenario 6: Print a Greeting for the User
 function greetUser(){
  console.log( `"Good day,user!"`);
-}
-greetUser()
+}greetUser()
 // Scenario 7: List Even Numbers from 1 to 10
 function showEvenNumbers(){
     for(var i=1;i<=10;i++){
         if(i%2==0){
-            console.log(i);
-        }
-    }
-}
-showEvenNumbers()
+         console.log(i);}}}showEvenNumbers()
 // Scenario 8: Display a Farewell Message
 function farewellMessage(){
 console.log(`"Thank you for visiting, goodbye!"`);
-}
-farewellMessage()
+}farewellMessage()
 // Scenario 9: Display Squares of Numbers from 1 to 5
 function name(){
 var i=1;
-for(var i=1;i<=5;i*=2){
-console.log(i);
-}
-}
+for(var i=1;i<=5;i*=2){console.log(i);}}
 name()
 // Scenario 10: Show a Welcome Message Three Times
 function repeatWelcome(){
  for(var i=1;i<=3;i++){
-    console.log("welcome!");
- }
-}
-repeatWelcome()
+ console.log("welcome!");}}repeatWelcome()
 
 
 
